0a9f2f768070c4991fc8a023437143c6853ad9ee
franco-melandri <franco.melandri@yoox.com>
2016-07-20 13:33:06 +0200

    Second step

--- fake/ObjectMapper.cs
+++ fake/ObjectMapper.cs
@@ -76,65 +76,67 @@ namespace fake
 
         public void fillAttributesRefactored(GetSearchResultResponse catalog)
         {
-            if (catalog.getRefinements() != null)
-            {
-                if (catalog.getRefinements().getFilters() != null)
-                {
-                    foreach (DynamicAttribute a in catalog.getRefinements().getFilters().getAttributes())
-                    {
-                        if (a.isSelected())
-                        {
-                            String code = a.getCode();
-                            foreach (DynamicAttribute aa in a.getAttributes())
-                            {
-                                if (!aa.getAttributes().isEmpty())
-                                {
-                                    foreach (DynamicAttribute aaa in aa.getAttributes())
-                                    {
-                                        if (aaa != null)
-                                        {
-                                            if (aaa.isSelected())
-                                            {
-                                                catalog.getSearchRequest().addValueToAttribute(code, aaa.getCode());
-                                            }
-                                        }
-                                    }
-                                }
+            if (catalog.getRefinements() == null) return;
 
-                                if (aa.isSelected())
+            if (catalog.getRefinements().getFilters() == null) return;
+
+
+            var dynamicAttributes = catalog.getRefinements().getFilters().getAttributes();
+            var searchRequest = catalog.getSearchRequest();
+
+            foreach (DynamicAttribute a in dynamicAttributes)
+            {
+                if (a.isSelected())
+                {
+                    String code = a.getCode();
+                    foreach (DynamicAttribute aa in a.getAttributes())
+                    {
+                        if (!aa.getAttributes().isEmpty())
+                        {
+                            foreach (DynamicAttribute aaa in aa.getAttributes())
+                            {
+                                if (aaa != null)
                                 {
-                                    catalog.getSearchRequest().addValueToAttribute(code, aa.getCode());
+                                    if (aaa.isSelected())
+                                    {
+                                        searchRequest.addValueToAttribute(code, aaa.getCode());
+                                    }
                                 }
                             }
+                        }
 
-                            // Check Refinements attributes under "ctgr" Attributes
-                            if (code == Filters.FILTER_CODE_CATEGORY)
+                        if (aa.isSelected())
+                        {
+                            searchRequest.addValueToAttribute(code, aa.getCode());
+                        }
+                    }
+
+                    // Check Refinements attributes under "ctgr" Attributes
+                    if (code == Filters.FILTER_CODE_CATEGORY)
+                    {
+                        if (a.getRefinements() != null && !a.getRefinements().isEmpty())
+                        {
+                            foreach (DynamicAttribute ar in a.getRefinements())
                             {
-                                if (a.getRefinements() != null && !a.getRefinements().isEmpty())
+                                code = ar.getCode();
+                                if (!ar.getAttributes().isEmpty())
                                 {
-                                    foreach (DynamicAttribute ar in a.getRefinements())
+                                    foreach (DynamicAttribute aar in ar.getAttributes())
                                     {
-                                        code = ar.getCode();
-                                        if (!ar.getAttributes().isEmpty())
+                                        if (aar != null)
                                         {
-                                            foreach (DynamicAttribute aar in ar.getAttributes())
+                                            if (aar.isSelected())
                                             {
-                                                if (aar != null)
-                                                {
-                                                    if (aar.isSelected())
-                                                    {
-                                                        catalog.getSearchRequest()
-                                                            .addValueToAttribute(code, aar.getCode());
-                                                    }
-                                                }
+                                                searchRequest
+                                                    .addValueToAttribute(code, aar.getCode());
                                             }
                                         }
-                                        if (ar.isSelected())
-                                        {
-                                            catalog.getSearchRequest().addValueToAttribute(code,
-                                                ar.getCode());
-                                        }
                                     }
+                                }
+                                if (ar.isSelected())
+                                {
+                                    searchRequest.addValueToAttribute(code,
+                                        ar.getCode());
                                 }
                             }
                         }
